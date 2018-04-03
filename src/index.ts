9ae19f6b391d80f3c0a3081578aa9428fc86ce92
masterpg <masterpg@livedeveloper.net>
2018-04-04 01:13:07 +0900

    promiseのpolyfill対応

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './app/app-view';
-import * as sw from './app/service-worker'
+import * as ES6Promise from "es6-promise";
+import * as sw from './app/service-worker';
 import { config } from './app/config';
+
+ES6Promise.polyfill();
 
 window.addEventListener('load', () => {
   console.log('environment:', config.environment);
